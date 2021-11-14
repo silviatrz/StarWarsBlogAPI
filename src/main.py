627afb4f0b3f306861d6a6e3e532410e78ded7d1
Silvia <silviatmnez@gmail.com>
2021-11-14 20:47:13 +0000

    first commit

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet, Character_Fav, Planet_Fav
 #from models import Person
 
 app = Flask(__name__)
@@ -38,6 +38,43 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/characters', methods=['GET'])
+def listCharacters():
+    list_characters = Character.query.all()
+    return jsonify([character.serialize() for character in list_characters]), 200
+
+@app.route('/characters/<int:character_id>', methods=['GET'])
+def getCharacter(character_id):
+    character = Character.query.filter_by(id=character_id).first()
+    return jsonify(character.serialize()), 200
+
+@app.route('/planets', methods=['GET'])
+def listPlanets():
+    list_planets = Planet.query.all()
+    return jsonify([planet.serialize() for planet in list_planets]), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def getPlanet(planet_id):
+    planet = Planet.query.filter_by(id=planet_id).first()
+    return jsonify(planet.serialize()), 200
+
+@app.route('/users', methods=['GET'])
+def listUsers():
+    list_users = User.query.all()
+    return jsonify([user.serialize() for user in list_users]), 200
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def getUser(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    return jsonify(user.serialize()), 200
+
+@app.route('/users/<int:user_id>/favorites', methods=['GET'])
+def listUserFavorites(user_id):
+    characterFavs = Character_Fav.query.all()
+    planetFavs = Planet_Fav.query.all()
+    userFavs = [characterFavs.query.filter_by(id=user_id) for fav in characterFavs]
+    return jsonify(user.serialize()), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,6 +4,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -17,3 +18,96 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    birth_year = db.Column(db.String(10), nullable=True)
+    gender = db.Column(db.String(25), nullable=True)
+    height = db.Column(db.Float, nullable=False)
+    skin_color = db.Column(db.String(25), nullable=False)
+    hair_color = db.Column(db.String(25), nullable=False)
+    eye_color = db.Column(db.String(25), nullable=False)
+    description = db.Column(db.String(300), nullable=False)
+    photo_url = db.Column(db.String(300), nullable=False)
+
+    def __repr__(self):
+        return '<Character %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "height": self.height,
+            "skin_color": self.skin_color,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+            "description": self.description,
+            "photo_url": self.photo_url,
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    climate = db.Column(db.String(25), nullable=False)
+    terrain = db.Column(db.String(25), nullable=False)
+    population = db.Column(db.Integer, nullable=True)
+    orbital_period = db.Column(db.Float, nullable=False)
+    rotation_period = db.Column(db.Float, nullable=False)
+    diameter = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(300), nullable=False)
+    photo_url = db.Column(db.String(300), nullable=False)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "population": self.population,
+            "orbital_period": self.orbital_period,
+            "rotation_period": self.rotation_period,
+            "diameter": self.diameter,
+            "description": self.description,
+            "photo_url": self.photo_url,
+        }
+
+class Character_Fav(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    id_character = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=False)
+    user = db.relationship('User')
+    character = db.relationship('Character')
+
+    def __repr__(self):
+        return '<Character_Fav %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "id_user": self.id_user,
+            "id_character": self.id_character,
+        }
+
+class Planet_Fav(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    id_planet = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=False)
+    user = db.relationship('User')
+    planet = db.relationship('Planet')
+
+    def __repr__(self):
+        return '<Planet_Fav %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "id_user": self.id_user,
+            "id_planet": self.id_planet,
+        }
+
